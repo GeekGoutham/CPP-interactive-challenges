0fb9f7722fe2b1d9688adab15ee69861ec733ade
Vignesh Goutham Ganesh <geekgoutham@gmail.com>
2016-07-28 10:29:32 -0700

    Adding log retrival and filter for unix time

--- quantil/query_tool.cpp
+++ quantil/query_tool.cpp
@@ -15,30 +15,31 @@ vector<string> split(string const &input, string &ip_k, int &id_d) {
 	return ret;
 }
 
-unsigned long unix_time(vector<string> query_vec){
+unsigned long unix_time(string &str_time){
 	time_t rawtime;
 	struct tm * timeinfo;
-	int from_y, from_m, from_d, from_h, from_min, to_y, to_m, to_d, to_h, to_min;
-	string buffer;
+	int y, m, d, h, min, date;
+	string buffer = str_time;
 	
-	buffer = query_vec[3] + " " + query_vec[4];
-	sscanf(buffer.c_str(), "%4d-%2d-%2d %2d:%2d", &from_y, &from_m, &from_d,
-				&from_h, &from_min);
+//	buffer = query_vec[3] + " " + query_vec[4];
+	sscanf(buffer.c_str(), "%4d-%2d-%2d %2d:%2d", &y, &m, &d,
+				&h, &min);
 
-	buffer = query_vec[5] + " " + query_vec[6];
-	sscanf(buffer.c_str(), "%4d-%2d-%2d %2d:%2d", &to_y, &to_m, &to_d, &to_h, &to_m);
+//	buffer = query_vec[5] + " " + query_vec[6];
+//	sscanf(buffer.c_str(), "%4d-%2d-%2d %2d:%2d", &to_y, &to_m, &to_d, &to_h, &to_m);
 	
 	time(&rawtime);
 	timeinfo = localtime ( &rawtime );
-	timeinfo->tm_year   = year - 1900;
-	timeinfo->tm_mon    = month - 1;    //months since January - [0,11]
-	timeinfo->tm_mday   = day;          //day of the month - [1,31] 
-	timeinfo->tm_hour   = hour +1;         //hours since midnight - [0,23]
+	timeinfo->tm_year   = y - 1900;
+	timeinfo->tm_mon    = m - 1;    //months since January - [0,11]
+	timeinfo->tm_mday   = d;          //day of the month - [1,31] 
+	timeinfo->tm_hour   = h +1;         //hours since midnight - [0,23]
 	timeinfo->tm_min    = min;          //minutes after the hour - [0,59]
 	timeinfo->tm_sec    = 0; 
 	date = mktime ( timeinfo );
 	date -= (8 *60 *60);     //To compenstate for PST (GMT -8) , Code uses local time for struct
 	
+	return date;
 
 }
 
@@ -62,8 +63,9 @@ int main(int argc, char* argv[]){
 	cout << obj.head->log_time << endl;
 */	
 
-	string inp_query, ip_key;
+	string inp_query, ip_key, buffer;
 	int id;
+	unsigned long ufrom_time, uto_time;
 	if(argc < 2){
 		cout << "The format to use is ./query.out <Path to directory of log>" <<endl;
 	}
@@ -89,7 +91,32 @@ int main(int argc, char* argv[]){
 			break;
 		else if(inp_query == "" | inp_query == " ") {cout<<">"; continue;}
 		else{
-			split(inp_query, ip_key, id);
+			vector<string> tokens = split(inp_query, ip_key, id);
+			buffer = query_vec[3] + " " + query_vec[4];
+			ufrom_time = unix_time(buffer);
+			buffer = query_vec[5] + " " + query_vec[6];
+			uto_time = unix_time(buffer);
+			
+
+			linkedlist obj = table.get_item(ip_key);
+			if (obj == NULL) {cout << "No matching entry found"; continue;}
+			
+			node *match_ptr = obj.head;
+			if (obj.head->log_time > uto_time) {cout<< "No logs found in the specified time frame"; continue;}
+			else if((obj.head->log_time > ufrom_time) && (obj.head->log_time < uto_time)){
+				while(match_ptr->log_time <= uto_time | match_ptr->next != NULL){
+					print_log(match_ptr);
+					match_ptr = match_ptr->next;
+				}
+			else if((obj.head->log_time <= ufrom_time) && (obj.tail->log_time => uto_time)){
+				while(match_ptr->log_time == ufrom_time){ match_ptr = match_ptr->next;}
+				while(match_ptr->log_time <= uto_time){
+					print_log(match_ptr);
+					match_ptr=match_ptr->next;
+				}
+			 }
+}
+			
 			
 		}
 
